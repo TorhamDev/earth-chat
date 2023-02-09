5ab1efe5616c626f8abf862f2dd74335275dd3f4
Torham <torhamdev@gmail.com>
2023-02-09 22:07:21 +0330

    Create basic consumer and setting up asgi configs

---
+++ chat/consumers.py
@@ -0,0 +1,36 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+import json
+
+class GlobalChatConsumer(AsyncJsonWebsocketConsumer):
+
+    async def connect(self):
+        self.channel_layer.group_add(
+            "global_chat",
+            self.channel_name,
+        )
+
+        await self.accept()
+
+
+
+    async def receive_json(self, content, **kwargs):
+        message = content["message"]
+
+        # Send message to all
+        await self.channel_layer.group_send(
+            "global_chat",
+            {
+                "type": "chat_message",
+                "message": message
+            },
+
+        )
+
+    async def disconnect(self, code=None):
+        return await super().disconnect(code)
+
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
---
+++ chat/routing.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+
+from chat import consumers
+
+websocket_urlpatterns = [
+    re_path(r"ws/chat/$", consumers.GlobalChatConsumer.as_asgi()),
+]
