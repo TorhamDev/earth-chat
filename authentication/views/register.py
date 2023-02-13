82607ffe46113010f5ae2afd17055322b1a10005
Torham <torhamdev@gmail.com>
2023-02-13 22:26:23 +0330

    Create an API for users register

--- authentication/views/register.py
+++ authentication/views/register.py
@@ -1,8 +1,28 @@
-from rest_framework.generics import CreateAPIView
-from django.contrib.auth.models import User
+from rest_framework.views import APIView
 from authentication.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
 
 
-class RegisterView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class RegisterView(APIView):
+
+    def post(self, request):
+
+        if "confirm_password" not in request.data:
+            raise ValidationError(
+                "You forgot confirm_password field.",
+                status.HTTP_400_BAD_REQUEST,
+            )
+
+        if request.data["confirm_password"] != request.data["password"]:
+            raise ValidationError(
+                "Confirm password doesn't match.",
+                status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
