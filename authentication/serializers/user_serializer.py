d99ceb4181e31fe0c7937cb778f6519b16ea0b48
Torham <torhamdev@gmail.com>
2023-02-13 22:25:54 +0330

    Add password field into user_serializer

--- authentication/serializers/user_serializer.py
+++ authentication/serializers/user_serializer.py
@@ -8,4 +8,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'password']
