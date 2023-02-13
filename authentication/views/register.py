d9e756891310e96b70043b1f51d2e95d6e9ad1cf
Torham <torhamdev@gmail.com>
2023-02-13 23:00:15 +0330

    Fix register api

--- authentication/views/register.py
+++ authentication/views/register.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from authentication.serializers import UserSerializer
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework import status
@@ -15,14 +16,25 @@ class RegisterView(APIView):
                 status.HTTP_400_BAD_REQUEST,
             )
 
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         if request.data["confirm_password"] != request.data["password"]:
             raise ValidationError(
                 "Confirm password doesn't match.",
                 status.HTTP_400_BAD_REQUEST,
             )
 
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        # Create user manualy
+        validation_data = serializer.validated_data
+
+        user = User(
+            username=validation_data.get("username"),
+            email=validation_data.get("email")
+        )
+
+        user.set_password(validation_data.get("password"))
+
+        user.save()
 
         return Response(serializer.data)
